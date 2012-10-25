8b6bdc715f0631ae7dda2a07519d1a875ca675ae
Jason Lingle <jasonlingle@yahoo.com>
2012-10-25 11:24:44 -0600

    Move configuration/common definitions to header.

--- src/abuhops.cxx
+++ src/abuhops.cxx
@@ -15,70 +15,12 @@
 
 #include <gmp.h>
 
+#include "common.hxx"
+
 using namespace std;
-
-/* SERVER CONFIGURATION.
- * To reconfigure the Abuhops server, redefine these, or pass equivilant
- * definitions in CXXFLAGS to make.
- */
-
-/** The file in which the shared secret is stored. */
-#ifndef SHARED_SECRET_FILE
-#define SHARED_SECRET_FILE "/usr/local/etc/abuhops/shared_secret"
-#endif
-
-/** The port number the server listens on */
-#ifndef PORT
-#define PORT 12545
-#endif
-
-/** The maximum number of seconds in the past or future a timestamp is allowed
- * to be. Since timestamps are provided by the authentication server, this
- * should be a small value.
- */
-#ifndef TIMESTAMP_VARIANCE
-#define TIMESTAMP_VARIANCE 15
-#endif
-/** The time in seconds after which an idle connection is considered gone. */
-#ifndet CONNECTION_TIMEOUT
-#define CONNECTION_TIMEOUT 300
-#endif
-/** The maximum length, in bytes, of a client advertisement. */
-#ifndef MAX_ADVERTISEMENT_SIZE
-#define MAX_ADVERTISEMENT_SIZE 128
-#endif
-/** Read random data from this file */
-#ifndef RANDOM_SOURCE
-#define RANDOM_SOURCE "/dev/urandom"
-#endif
-
-
-
-#define ELGAMAL_BASE "2"
-#define ELGAMAL_MODULUS "444291e51b3ea5fd16673e95674b01e7b"
-#define SIGNATURE_CHUNK_SIZE 17
-
-typedef unsigned char byte;
 
 static mpz_t elGamalModulus, elGamalModulusMinusOne, elGamalBase;
 static FILE* randomSource;
-
-template<typename Address>
-Address convertAddress(const asio::ip::address&) {
-  return Address::is_not_an_understood_address_type;
-}
-
-template<>
-asio::ip::address_v4
-convertAddress<asio::ip::address_v4>(const asio::ip::address& addr) {
-  return addr.to_v4();
-}
-
-template<>
-asio::ip::address_v6
-convertAddress<asio::ip::address_v6>(const asio::ip::address& addr) {
-  return addr.to_v6();
-}
 
 template<typename Address>
 class Abuhops {
