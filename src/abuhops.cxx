ea6816da8b95212bbaeb53ac81a7b5c01666790c
Jason Lingle <jasonlingle@yahoo.com>
2012-11-04 18:05:19 -0700

    Implement main().

--- src/abuhops.cxx
+++ src/abuhops.cxx
@@ -3,5 +3,15 @@
 #endif
 
 #include "common.hxx"
+#include "service.hxx"
+#include "clientelle.hxx"
 
 using namespace std;
+
+int main(void) {
+  if (!initService())
+    return 1;
+
+  addMaintFunc(cleanClientMaps);
+  runListenReadLoop();
+}
